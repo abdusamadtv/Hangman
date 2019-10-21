646bee0c5338148dcd4969c8ea6143fc5c4ffaad
Abdusamad <desire616sense@gmail.com>
2019-10-21 23:41:29 +0500

    Extra tools have been removed

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view />
+    <h1>Hangman</h1>
   </div>
 </template>
 
@@ -14,18 +11,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
